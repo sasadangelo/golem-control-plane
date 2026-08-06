14991310f03b1d66a179801a9f29b2b413f880dd
Salvatore D'Angelo <sasadangelo@gmail.com>
2026-08-06 12:33:36 +0200

    chore: initial project scaffold

---
+++ tests/test_hello.py
@@ -0,0 +1,7 @@
+from golem.hello import say_hello
+
+
+def test_say_hello() -> None:
+    assert say_hello(name="World") == "Hello, World!"
+    assert say_hello(name="Alice") == "Hello, Alice!"
+    assert say_hello(name="Bob") != "Hello, Alice!"
